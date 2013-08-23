e6c2f528b0c387703aeaa0cad106cf0fb787487c
martin2387 <cheenote@hotmail.com>
2013-08-23 12:13:43 -0400

    Arty is so lame!!!

--- src/TestBloodType.java
+++ src/TestBloodType.java
@@ -2,8 +2,21 @@
 public class TestBloodType {
 
 	public static void main(String[] args) {
+		// Declaring variables....
+		BloodType arty = new BloodType("F", '-');
+		BloodType ian = new BloodType();
+		BloodType cj = new BloodType("DJ");
+		BloodType morley = new BloodType('-');
 		
-
+		// Testing Constructor methods
+		arty.setBloodType("D");
+		arty.setRhFactor('-');
+		
+		// Displaying the output info of patients.
+		System.out.println("Arty's blood type is" + arty.getBloodType() + arty.getRhFactor());
+		System.out.println("Ian's blood type is " + ian.getBloodType() + ian.getRhFactor());
+		System.out.println("CJ's blood type is " + cj.getBloodType() + cj.getRhFactor());
+		System.out.println("Morley's blood type is " + morley.getBloodType() + morley.getRhFactor());
 	}
 
 }
--- src/TestPatient.java
+++ src/TestPatient.java
@@ -2,8 +2,30 @@
 public class TestPatient {
 
 	public static void main(String[] args) {
+		// Declaring methods and variables.
+		BloodType artysBloodType = new BloodType("O",'+');
+		Patient arty = new Patient(87799, 37, artysBloodType);
+		Patient ian = new Patient();
+		Patient cj = new Patient();
+		Patient morley = new Patient();
 		
-
+		// Testing the constructor methods
+		ian.setPatientAge(23);
+		ian.setPatientIdNumber(148713);
+		BloodType iansBloodType = new BloodType("A", '+');
+		ian.setBloodType(iansBloodType);
+		
+		// Displays the output for Patients Info...		
+		System.out.println("Arty's ID number is " + arty.getPatientIdNumber() + ", he is " + arty.getPatientAge()
+				+ " years old. His blood type is " + arty.getBloodType());
+		System.out.println("ian's ID number is " + ian.getPatientIdNumber() + ", he is " + ian.getPatientAge()
+				+ " years old. His blood type is " + ian.getBloodType());
+		System.out.println("cj's ID number is " + cj.getPatientIdNumber() + ", he is " + cj.getPatientAge()
+				+ " years old. His blood type is " + cj.getBloodType());
+		System.out.println("morley's ID number is " + morley.getPatientIdNumber() + ", he is " + morley.getPatientAge()
+				+ " years old. His blood type is " + morley.getBloodType());
+	
+		
 	}
 
 }
